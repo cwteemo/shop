2968f4ae018229cf9090dd0663c568944b1ee052
cw1993teemo@163.com <cw1993teemo@163.com>
2022-11-01 21:07:57 +0800

    new  添加后端项目

---
+++ admin-suppermail/route/app.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\facade\Route;
+
+Route::rest([
+    'read'      => ['GET', '/read', 'read'],
+    'edit'      => ['GET', '/edit', 'edit'],
+    'update'    => ['PUT', '', 'update'],
+    'delete'    => ['DELETE', '', 'delete']
+]);
+
+Route::resource('project',      'Project');
